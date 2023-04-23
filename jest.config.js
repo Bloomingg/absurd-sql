6b18b17604e72690e39b5987bb782191a8285d69
Bloomingg <1293499952@qq.com>
2023-04-23 12:59:41 +0800

    1

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  transformIgnorePatterns: [
-    // Change MODULE_NAME_HERE to your module that isn't being compiled
-    '/node_modules/(?!perf-deets).+\\.js$'
-  ],
-  moduleNameMapper: {
-    'perf-deets': 'perf-deets/noop'
-  }
-};
